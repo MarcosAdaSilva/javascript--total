f2620e10959f25f358123058eb0a82f15b08ad22
MarcosAdaSilva <marcaofly@gmail.com>
2022-08-15 20:04:46 -0300

    usando strings

---
+++ 6 strings/js/scripts.js
@@ -0,0 +1,25 @@
+var nome = "Marcos";
+var sobreNome = "Antônio";
+
+var nomeCompleto = nome + " " + sobreNome; // concatenando 
+
+
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(nomeCompleto);
+
+console.log(typeof  "5.292929");
+
+var frase = 'Esta é uma frase complexa';
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+document.write('Ele disse "Olá mundo!"');
+document.write("Ele disse 'Olá mundo!'");
+
+console.log("Este número:" + nome)
