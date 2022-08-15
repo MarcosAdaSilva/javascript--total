2b68a6a2ccf6d9fe211455da4a55c1f492aefc77
MarcosAdaSilva <marcaofly@gmail.com>
2022-08-15 18:29:32 -0300

    Particularidades do JS

---
+++ 3 Particularidades js/js/scripts.js
@@ -0,0 +1,23 @@
+console.log("Primeiro");
+
+console.log("Último");
+
+console.log("Mais um console.log!");
+
+// Testando uma variável tipagem fraca.
+// Declarando uma variável
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
+
+/*
+Este
+é
+um 
+comentário
+de 
+múltiplas
+linhas
+*/
