00abecf6c8ac356f5d4bf3ae87234038b42635c0
MarcosAdaSilva <marcaofly@gmail.com>
2022-08-15 19:29:55 -0300

    declarando variáveis

---
+++ 4 Variaveis/js/scripts.js
@@ -0,0 +1,47 @@
+var teste = 1;
+
+nome = "João";
+
+console.log(teste);
+
+teste = 'Marcos e João';
+
+console.log(teste)
+
+console.log(nome);
+
+// caracteres especiais apenas $ e _
+var $nome = "ASD";
+var _nome = 'ASD';
+
+console.log($nome);
+console.log(_nome);
+
+// não começar uma variável com número.
+var nome5 = 'ASD3';
+
+console.log(nome5)
+
+// CamelCase
+var meuPrimeiroNome = "Marcos";
+
+console.log(meuPrimeiroNome); //camelCase
+
+// Outros tipos de declaração de variáveis
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+// Variáveis sem valor atribuido
+
+var meuNome;
+console.log(meuNome);// = undefined
+
+// atribuindo valor para variável
+meuNome = "Marcos";
+console.log(meuNome);
+
+
+// nunca mudar o valor de uma variável
