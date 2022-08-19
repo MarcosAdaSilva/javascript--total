682238cb73d351bf7e4cc6bfef6e3b4666d8df57
MarcosAdaSilva <marcaofly@gmail.com>
2022-08-18 23:14:14 -0300

    Estudando

---
+++ 13 Operadores de comparação/js/scripts.js
@@ -0,0 +1,15 @@
+var idade = 16;
+var idademinima = 18;
+
+console.log("Antes do if");
+
+// if se for true o app executa, se for false passa direto.
+if (idade > idademinima) {
+  console.log("Pode fazer a carteira de habilitação");
+}
+
+if (idade > 15) {
+  console.log("Precisa esperar um pouco mais");
+}
+
+console.log("Depois do if");
