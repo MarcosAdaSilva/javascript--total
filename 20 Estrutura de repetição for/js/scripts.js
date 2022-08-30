169ee7d38abed122b97ea7e3e1e1fe7a405e83a7
MarcosAdaSilva <marcaofly@gmail.com>
2022-08-29 23:47:50 -0300

    Estudando

--- 20 Estrutura de repetição for/js/scripts.js
+++ 20 Estrutura de repetição for/js/scripts.js
@@ -1,34 +1,3 @@
-var x = 1;
-var y = 2;
-
-// soma
-console.log((x = x + y));
-console.log((x += y));
-
-// subtração
-console.log((x -= y));
-
-// multiplicação
-console.log((x *= y));
-
-// divisão
-console.log((x /= y));
-
-// loops
-console.log(x++);
-console.log(x--);
-
-while (x <= 100) {
-  console.log(x);
-  x *= 2;
-
-  console.log(x);
-}
-
-var j = 5;
-
-while (x >= 0) {
-  console.log(x);
-
-  x -= j;
+for (var i = 0; i < 10; i++) {
+  console.log("Repetindo for: " + i);
 }
