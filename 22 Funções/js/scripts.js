3d9325bc94c705d7fe48317cf91570ec07078faf
MarcosAdaSilva <marcaofly@gmail.com>
2022-08-30 23:04:37 -0300

    Estudando

---
+++ 22 Funções/js/scripts.js
@@ -0,0 +1,14 @@
+function primeiraFuncao() {
+  console.log("Hollo world das funções");
+}
+primeiraFuncao();
+primeiraFuncao();
+primeiraFuncao();
+primeiraFuncao();
+primeiraFuncao();
+// As funções podem ser chamadas e repetidas quantas vezes queiser.
+function dizerNome(nome) {
+  console.log("O nome é :" + nome);
+}
+
+dizerNome("Marcos");
