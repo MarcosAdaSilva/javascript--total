e58523f265fb86677a945426fc0d775ae1ccd264
MarcosAdaSilva <marcaofly@gmail.com>
2022-09-03 10:21:16 -0300

    Estudando

---
+++ 25 Métodos numéricos/js/scripts.js
@@ -0,0 +1,29 @@
+// parseFloat 10.0
+
+console.log(parseFloat("12.999"));
+console.log(Number.parseFloat("12.999"));
+
+// parseInt
+
+console.log(parseInt("10"));
+console.log(parseInt("16.96"));
+
+// toFixed
+
+console.log((23.515156).toFixed(1));
+console.log((23.515156).toFixed(2));
+console.log((23.515156).toFixed(3));
+
+// isNaN
+
+console.log(isNaN("teste"));
+console.log(isNaN(12));
+console.log(isNaN("14"));
+
+// MAS_VALUE e MIN_VALUE
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+console.log(2.7976931348623157e308);
+console.log(1e-322);
