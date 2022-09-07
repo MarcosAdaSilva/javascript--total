0727f7065de8a292e10c390d15688893132a01f5
MarcosAdaSilva <marcaofly@gmail.com>
2022-09-06 23:06:04 -0300

    Estudando

---
+++ 27 Métodos de string Parte 2/js/scripts.js
@@ -0,0 +1,18 @@
+// tolowerCase e toUpperCase
+
+var frase = "Esta é a frase que vamos manipular";
+
+var fraseCaixaAlta = frase.toUpperCase();
+
+console.log(fraseCaixaAlta);
+
+console.log(frase.toLowerCase());
+
+// trim
+
+var nome = "       Marcos         ";
+
+var nomeTrim = nome.trim();
+
+console.log(nome);
+console.log(nomeTrim);
