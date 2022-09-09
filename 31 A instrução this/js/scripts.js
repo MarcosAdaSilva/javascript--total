4b88729b27293bd40ae961c06108ffa8bc08fd20
MarcosAdaSilva <marcaofly@gmail.com>
2022-09-08 23:57:05 -0300

    Estudando

---
+++ 31 A instrução this/js/scripts.js
@@ -0,0 +1,18 @@
+let pessoa = {
+  nome: "Marcos",
+  idade: 29,
+  falar: function () {
+    console.log("olá, tudo bem?");
+  },
+  soma: function (a, b) {
+    return a + b;
+  },
+};
+
+console.log(pessoa.nome);
+
+pessoa.falar();
+
+var soma = pessoa.soma(2, 2);
+
+console.log(soma);
