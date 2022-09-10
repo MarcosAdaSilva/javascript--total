5088675e95e866fb29405fd0cc2cc29e714782f8
MarcosAdaSilva <marcaofly@gmail.com>
2022-09-09 23:14:34 -0300

    Estudando

--- 32 DOM/js/scripts.js
+++ 32 DOM/js/scripts.js
@@ -5,7 +5,13 @@ console.log(titulo);
 
 var lis = document.getElementsByTagName("li");
 
-console.log(lis[3]);
+console.log(lis);
+// id
 var paragrafo = document.getElementById("paragrafo");
 
 console.log(paragrafo);
+
+// class
+
+var itensDaLista = document.getElementsByClassName("item");
+console.log(itensDaLista);
