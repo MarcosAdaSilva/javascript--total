9bcccc485ca0784cd9359e97f4ff7640aa2093f9
MarcosAdaSilva <marcaofly@gmail.com>
2022-09-09 23:43:38 -0300

    estudando

--- 33 Formas de acessar o DOM/js/scripts.js
+++ 33 Formas de acessar o DOM/js/scripts.js
@@ -13,8 +13,18 @@ var itensQuery2 = document.querySelectorAll("#lista .item");
 
 console.log(itensQuery2);
 
-// queryselector
+// queryselector item
 
 var itensQuery3 = document.querySelector("#lista .item");
 
 console.log(itensQuery3);
+
+// queryselector elemento
+
+var lista = document.querySelector("#lista");
+
+console.log(lista);
+
+var span = document.querySelector("#paragrafo span");
+
+console.log(span);
