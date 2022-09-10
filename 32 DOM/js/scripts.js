74dbadc64da18860b2e6e4f5747da9760dbe8f86
MarcosAdaSilva <marcaofly@gmail.com>
2022-09-09 23:08:05 -0300

    Estudando

---
+++ 32 DOM/js/scripts.js
@@ -0,0 +1,11 @@
+// tag
+var titulo = document.getElementsByTagName("h1")[0];
+
+console.log(titulo);
+
+var lis = document.getElementsByTagName("li");
+
+console.log(lis[3]);
+var paragrafo = document.getElementById("paragrafo");
+
+console.log(paragrafo);
