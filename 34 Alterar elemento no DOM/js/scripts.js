1b3f09e72ce224d94191d8c12f261fd2a3a1f5bf
MarcosAdaSilva <marcaofly@gmail.com>
2022-09-12 23:50:24 -0300

    estudando

---
+++ 34 Alterar elemento no DOM/js/scripts.js
@@ -0,0 +1,7 @@
+// selecionar elementos
+var title = document.querySelector("#title");
+
+// innerHTML
+title.innerHTML = "Testando o texto alterado";
+
+// textContent -> mais utilizado, recomendado e performatico
