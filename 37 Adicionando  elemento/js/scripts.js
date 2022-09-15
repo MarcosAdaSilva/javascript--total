df773198ef40f209a75ecfb216b795ec776afe89
MarcosAdaSilva <marcaofly@gmail.com>
2022-09-14 23:43:17 -0300

    Estudando

---
+++ 37 Adicionando  elemento/js/scripts.js
@@ -0,0 +1,18 @@
+// criar elemento
+var el = document.createElement("div");
+
+el.classList = "div-criada";
+
+console.log(el);
+
+// inserindo elemento filho
+
+var container = document.querySelector("#container");
+
+container.appendChild(el);
+
+// inserBefore - inserindo antes
+
+var el2 = document.createElement("div");
+
+el2;
