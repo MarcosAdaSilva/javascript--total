b3053aad3ff204f9c5f2468397157a324a798928
MarcosAdaSilva <marcaofly@gmail.com>
2022-09-16 00:07:06 -0300

    Estudando

--- 38 Trocando elementos/js/scripts.js
+++ 38 Trocando elementos/js/scripts.js
@@ -4,3 +4,18 @@ var el = document.createElement("h3");
 el.classList = "testando-classe";
 
 var texto = document.createTextNode("Este é o texto do h3");
+
+el.appendChild(texto);
+
+console.log(el);
+
+// selecionar o elemento que quer trocar
+var title = document.querySelector("#title");
+
+console.log(title);
+
+// selecionar o pai do elemento
+var pai = title.parentNode;
+
+// trocar os elementos
+pai.replaceChild(el, title);
