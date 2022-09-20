8e0212635818c9bb53a6c3caf9df00de91494b15
MarcosAdaSilva <marcaofly@gmail.com>
2022-09-19 23:51:35 -0300

    Estudando

--- 39 Adicionando atributo/js/scripts.js
+++ 39 Adicionando atributo/js/scripts.js
@@ -3,3 +3,7 @@ var title = document.querySelector("#title");
 title.setAttribute("class", "testando-atributo");
 
 console.log(title);
+
+var btn = document.querySelector("#btn");
+
+btn.setAttribute("disabled", "disabled");
