45aba16856dca44db66314159a07e6b0b6b962d2
MarcosAdaSilva <marcaofly@gmail.com>
2022-09-20 00:35:23 -0300

    Estudando

--- 40 Adicionando CSS com JS/js/scripts.js
+++ 40 Adicionando CSS com JS/js/scripts.js
@@ -1,6 +1,10 @@
 // selecionando elemento
 var title = document.querySelector("#title");
 
+var subtitle = document.querySelector(".subtitle");
 // adicinando o estilo
 
 title.style.color = "red";
+title.style.backgroundColor = "yellow"; // camelCase
+
+subtitle.style.cssText = "color: blue; background-color: pink; font-size: 50px";
