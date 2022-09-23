10306da7eee780c4e0cdef4dc1710f9224a71f6b
MarcosAdaSilva <marcaofly@gmail.com>
2022-09-22 23:16:07 -0300

    Estudando

--- 47 Evento mouseover e mouseout/js/scripts.js
+++ 47 Evento mouseover e mouseout/js/scripts.js
@@ -10,3 +10,19 @@ title.addEventListener("mouseover", function () {
 title.addEventListener("mouseout", function () {
   this.style.backgroundColor = "white";
 });
+
+// afetar outro elemento com mouseover
+
+var subtitle = document.querySelector(".subtitle");
+
+subtitle.addEventListener("mouseover", function () {
+  var legenda = document.querySelector("#legenda");
+
+  legenda.classList.remove("hide");
+});
+
+subtitle.addEventListener("mouseout", function () {
+  var legenda = document.querySelector("#legenda");
+
+  legenda.classList.add("hide");
+});
