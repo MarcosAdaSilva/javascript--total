bc2c57d2437c636d883b1e454c3ad1b75bf9335c
MarcosAdaSilva <marcaofly@gmail.com>
2022-09-26 23:58:44 -0300

    Estudando

--- 49 Projeto/js/scripts.js
+++ 49 Projeto/js/scripts.js
@@ -1,5 +1,13 @@
+// função que adiciona tarefa
+function addTask() {
+  // título da tarefa
+}
+
+// evento de adicinar tarefa
 const addBtn = document.querySelector("#add-btn");
 
 addBtn.addEventListener("click", function (e) {
   e.preventDefault();
+
+  addTask();
 });
